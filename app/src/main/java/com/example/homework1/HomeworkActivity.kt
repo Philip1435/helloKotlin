b948e3007d2692def2452d619b072f123932de21
Philip1435 <6000189@kvis.ac.th>
2023-11-21 22:24:36 +0700

    first look on homework1

---
+++ app/src/main/java/com/example/homework1/HomeworkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.homework1
+
+import android.os.Bundle
+import android.widget.GridLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw1)
+    }
+}
